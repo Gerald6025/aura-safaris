9582e56fb189756ad0458c83f447a4a5dd657d83
Gerald6025 <geraldgchibanda6025@gmail.com>
2025-11-13 11:52:52 +0200

    second

--- components/why-choose.tsx
+++ components/why-choose.tsx
@@ -48,11 +48,11 @@ const WhyChoose = () => {
           <div className="h-0.5 w-[200px] bg-[#cc9933] mt-4" />
         </div>
 
-        <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:gap-80 gap-10 px-4 justify-items-center">
+        <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:gap-70 gap-10 px-4 justify-items-center">
           {features.map((item, idx) => (
             <div
               key={idx}
-              className="group cursor-pointer rounded-xl border border-gray-200 flex items-center justify-center flex-col bg-white p-6 w-[330px] h-[300px] shadow-[0_6px_0_rgba(0,0,0,0.2)] hover:shadow-[0_10px_10px_rgba(0,0,0,0.25)] transition-shadow"
+              className="group cursor-pointer rounded-xl border border-gray-200 flex items-center justify-center flex-col bg-white p-6 w-[300px] h-[300px] shadow-[0_6px_0_rgba(0,0,0,0.2)] hover:shadow-[0_10px_10px_rgba(0,0,0,0.25)] transition-shadow"
             >
               <div className="mb-4">{item.icon}</div>
               <h3 className="text-[21px] font-semibold text-black w-[250px] h-[29px] text-center transform transition-transform duration-500 ease-out group-hover:translate-y-1">
